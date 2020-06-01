b41351b474960273eb36c91af3dcf54936c4cf3d
JesusGH123 <a01423730@itesm.mx>
2020-05-31 21:23:25 -0500

    Todos los electrodos se muestran

--- BraiNerdPanel.py
+++ BraiNerdPanel.py
@@ -1,24 +1,25 @@
-#Esta clase creea paneles  o pantallas que se visualizan con botones, graficas, imagenes, etc...
+#Esta clase crea paneles  o pantallas que se visualizan con botones, graficas, imagenes, etc...
 import wx
+import wx.xrc as xrc
 import os
 import numpy as np
 from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
 from matplotlib.figure import Figure 
 
-from FileInOut import FileInOut
 
+import wx.lib.agw.buttonpanel as BP
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+from matplotlib.widgets import Button
+import  cStringIO
 
-#file = FileInOut()
-#file.ReadData()
+
 
 class BraiNerdPanel(wx.Panel):
    
     def __init__(self,parent):
         wx.Panel.__init__(self,parent)
-        self.SetBackgroundColour(wx.Colour(121,255,255,0))
         self.r = 0
         self.theta = 0
         
@@ -27,52 +28,50 @@ class BraiNerdPanel(wx.Panel):
     def ShowMenuPanel(self):
         self.SetBackgroundColour(wx.Colour(121,255,255,0))
         self.inicio = wx.Button(self, label="Inicio",pos=(200,90), size=(140, 25))
-        self.configuracion = wx.Button(self, label='Configuracion',pos=(350, 90), size=(140, 25))
+        self.abrir = wx.Button(self, label='Abrir',pos=(350, 90), size=(140, 25))
         self.guardar = wx.Button(self, label='Guardar',pos=(500,90), size=(140, 25))
         self.puntosMon = wx.Button(self, label='Puntos de Monitoreo',pos=(650, 90), size=(140, 25))
+        
+        bitmap = wx.Bitmap('LogoB.jpg')
+        image = wx.ImageFromBitmap(bitmap)
+        image = image.Scale(80, 80, wx.IMAGE_QUALITY_HIGH)
+        result = wx.BitmapFromImage(image)
+        self.control = wx.StaticBitmap(self, -1, result)
+        self.control.SetPosition((10, 10))
+       
+    """
         bitmap = wx.Bitmap('LogoB.jpg')
         image = wx.ImageFromBitmap(bitmap)
         image = image.Scale(80, 80, wx.IMAGE_QUALITY_HIGH)
         result = wx.BitmapFromImage(image)
         self.control = wx.StaticBitmap(self, -1, result)
         self.control.SetPosition((450, 0))
-        self.Bind(wx.EVT_BUTTON, self.OnButtonClickInicio, self.inicio) 
-        self.Bind(wx.EVT_BUTTON, self.OnButtonClickConfiguracion, self.configuracion) 
-        self.Bind(wx.EVT_BUTTON, self.OnButtonClickGuardar, self.guardar) 
-        self.Bind(wx.EVT_BUTTON, self.OnButtonClickPuntos, self.puntosMon)
+"""
+    def InicioButton(self):
+        return self.inicio
+    def AbrirButton(self):
+        return self.abrir
+    def GuardarButton(self):
+        return self.guardar
+    def PuntosMonButton(self):
+        return self.puntosMon
+   
 
-
-    def OnButtonClickInicio(self, event):
-        self.SetBackgroundColour('Yellow')
-        self.Refresh()
-    
-    def OnButtonClickConfiguracion(self, event):
-        self.SetBackgroundColour('Blue')
-        self.Refresh()
-  
-    def OnButtonClickGuardar(self, event):
-        self.SetBackgroundColour('Red')
-        self.Refresh()
     def UpdateMenu(self):
         X,Y = self.GetSize()
         Y = Y-30
         if(X>=0):
             iniX = (X/2)-300
-            confX = (X/2)-150
+            abrirX = (X/2)-150
             guaX = (X/2)+10
             ptsMonX = (X/2)+160
             self.inicio.SetPosition(wx.Point(iniX,Y))
-            self.configuracion.SetPosition(wx.Point(confX,Y))
+            self.abrir.SetPosition(wx.Point(abrirX,Y))
             self.guardar.SetPosition(wx.Point(guaX,Y))
             self.puntosMon.SetPosition(wx.Point(ptsMonX,Y))
             self.control.SetPosition(((X/2)-40, 0))
        
-
-       
-
-    def OnButtonClickPuntos(self, event):
-        self.SetBackgroundColour('Pink')
-        self.Refresh()
+  
 
     def SetAngleElectrode(self, angle):
         self.theta = angle
@@ -87,7 +86,7 @@ class BraiNerdPanel(wx.Panel):
     def PutGraphIn(self):
         self.fig = Figure(figsize=[2,2]) #Grafica tamano
         self.ax = self.fig.add_subplot(111, projection='polar') #puedo probar con ',facecolor='black'
-        self.canvas = FigureCanvas(self, -1, self.fig)
+        self.canvas = FigureCanvas(self, 1, self.fig)
         self.sizer  = wx.BoxSizer(wx.HORIZONTAL)
         self.sizer.Add(self.canvas, 1, wx.EXPAND)
         self.SetSizer(self.sizer)
@@ -105,36 +104,40 @@ class BraiNerdPanel(wx.Panel):
         self.point.remove()
         self.point = self.ax.scatter(self.GetAngleElectrode(),self.GetRElectrode(), c=np.zeros(32), s=(50), cmap='hsv', alpha = intensity) #dibuja la grafica
 
-
+    def GetCanvas(self):
+        return self.canvas
+    def GetPanel(self):
+        return self
+    #inicializa el boton 'X' de todos los cuadrantes
+    def ShowMinimizarButton(self):
+        self.minimizar = Button(self.fig.add_axes([.95,.95,.05,.05]),"X")
+    #retorna la varibale del boton 'X' para otra clase que lo quiera controlar
+    def GetMinimizarButton(self):
+        return self.minimizar
     #Mostrar el panel que contiene al primer cuadrante con la grafica que representa las ondas Theta.
-    def ShowThetaPanel(self,angle,r):
-        self.SetAngleElectrode(angle)
-        self.SetRElectrode(r)
+    def ShowThetaPanel(self,info):
+        self.SetAngleElectrode(info[2])
+        self.SetRElectrode(info[1])
         self.PutGraphIn()
-        self.Bind(wx.EVT_LEFT_UP, self.onClick)
-
-    def onClick(self, event):
-        print ("click")
-
-        
+        self.ax.set_title("Theta Graph")
     #Mostrar el panel que contiene al segundo cuadrante con la grafica que representa las ondas Alpha.
-    def ShowAlphaPanel(self,angle,r):
-        self.SetAngleElectrode(angle)
-        self.SetRElectrode(r)
+    def ShowAlphaPanel(self,info):
+        self.SetAngleElectrode(info[2])
+        self.SetRElectrode(info[1])
         self.PutGraphIn()
-
+        self.ax.set_title("Alpha Graph")
     #Mostrar el panel que contiene al tercer cuadrante con la grafica que representa las ondas Beta.
-    def ShowBetaPanel(self,angle,r):
-        self.SetAngleElectrode(angle)
-        self.SetRElectrode(r)
+    def ShowBetaPanel(self,info):
+        self.SetAngleElectrode(info[2])
+        self.SetRElectrode(info[1])
         self.PutGraphIn()
-
+        self.ax.set_title("Beta Graph")
     #Mostrar el panel que contiene al cuarto cuadrante con la grafica que representa las ondas Delta.
-    def ShowDeltaPanel(self,angle,r):
-        self.SetAngleElectrode(angle)
-        self.SetRElectrode(r)
+    def ShowDeltaPanel(self,info):
+        self.SetAngleElectrode(info[2])
+        self.SetRElectrode(info[1])
         self.PutGraphIn()
-
+        self.ax.set_title("Delta Graph")
 
     
 
--- FileInOut.py
+++ FileInOut.py
@@ -1,15 +1,45 @@
 #Esta clase se encarga de leer y guardar datos en el archivo
 import csv
+from csv import reader
+import wx
+import pandas as pd
+import string
 class FileInOut():
     def __init__(self):
-        global file_location_Input
-        file_location_Input = "./InData.csv"
-
+        self.file_location_Input = ""
+        self.rowCount = 0
 	#Este metodo lee el archivo
     def ReadData(self):
-        file = open(file_location_Input, newline='')
-        spamreader = csv.reader(file, delimiter=' ', quotechar='|')
-        for row in spamreader:
-            print(', '.join(row))
+        df = pd.read_csv(self.file_location_Input, skiprows=self.rowCount,nrows=0)
+        row = df.iloc[:,:]
+        row = list(row)
+        newList = []
+        a=1
+        for i in range (0,len(row)/4):
+            newList.append(row[a:a+4])
+            a+=4
+        #print (newList)
+        self.rowCount+=1
+        return zip(*newList)
+      
+    
+    def OnOpen(self):
+        frame = wx.Frame(None, -1, 'win.py')
+        frame.SetDimensions(0,0,200,50)
 
+        # Create open file dialog
+        openFileDialog = wx.FileDialog(frame, "Seleccion de archivo", "", "", 
+            "Python files (*.csv)|*.csv", 
+            wx.FD_OPEN | wx.FD_FILE_MUST_EXIST)
+
+        openFileDialog.ShowModal()
+        self.file_location_Input = openFileDialog.GetPath()
+        #print(openFileDialog.GetPath())
+        openFileDialog.Destroy()
+        #return self.ReadData(self.file_location_Input)
+    def SelectedFile(self):
+        if (self.file_location_Input == ""):
+            return False
+        else:
+            return True
 
